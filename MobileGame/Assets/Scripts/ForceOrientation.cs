8a63a78b06a704c6608cf425cef75b36bb58c7f1
Stijn van der Neut <stijn.jasper@hotmail.com>
2019-11-13 15:27:01 +0100

    Added a knight and a lich model - Made the PlayerController stop using the rigidbody to move

---
+++ MobileGame/Assets/Scripts/ForceOrientation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceOrientation : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log(Screen.width + "      " + Screen.height);
+        }
+    }
+}
---
+++ MobileGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    Vector3 direction;
+    Vector3 origin;
+    Rigidbody rigidBody;
+
+    [SerializeField] private GameObject walkingPrefab;
+    [SerializeField] private GameObject canvas;
+
+    GameObject joystickObject;
+
+    Animator animator;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        CheckForMouseInput();
+    }
+
+    void FixedUpdate()
+    {
+        if (direction.magnitude > 0)
+        {
+            transform.position = transform.position - direction * moveSpeed * Time.fixedDeltaTime;
+            //rigidBody.MovePosition(transform.position - direction * 5f * Time.fixedDeltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-direction, Vector3.up), Time.fixedDeltaTime * 10f);
+        }
+    }
+
+    void CheckForMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            origin = Input.mousePosition;
+
+            GameObject obj = Instantiate(walkingPrefab, origin, Quaternion.identity, canvas.GetComponent<RectTransform>());
+            joystickObject = obj;
+
+            animator.SetBool("isRunning", true);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            direction = origin - Input.mousePosition;
+            direction.z = direction.y;
+            direction.y = 0f;
+            direction.Normalize();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            animator.SetBool("isRunning", false);
+            direction = Vector3.zero;
+            Destroy(joystickObject);
+        }
+    }
+}
