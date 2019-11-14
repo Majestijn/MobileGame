92557329ee67c7ddf0aa6d5d425d51e37910fe00
Stijn van der Neut <stijn.jasper@hotmail.com>
2019-11-14 15:57:26 +0100

    Tried something with shaders to draw on texture

--- MobileGame/Assets/Scripts/DrawSpellScript.cs
+++ MobileGame/Assets/Scripts/DrawSpellScript.cs
@@ -4,42 +4,72 @@ using UnityEngine;
 
 public class DrawSpellScript : MonoBehaviour
 {
-    private LineRenderer lineRenderer;
-
-    private bool isDrawing = false;
-
+    public int resolution = 512;
+    Texture2D whiteMap;
+    public float brushSize;
+    public Texture2D brushTexture;
+    Vector2 stored;
+    public static Dictionary<Collider, RenderTexture> paintTextures = new Dictionary<Collider, RenderTexture>();
     void Start()
     {
-        lineRenderer = GetComponent<LineRenderer>();
+        CreateClearTexture();// clear white texture to draw on
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            isDrawing = true;
-            StartCoroutine(DrawShape());
-        }
 
-        if (Input.GetMouseButtonUp(0))
+        Debug.DrawRay(transform.position, Vector3.down * 20f, Color.magenta);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        //if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) // delete previous and uncomment for mouse painting
         {
-            isDrawing = false;
+            Collider coll = hit.collider;
+            if (coll != null)
+            {
+                if (!paintTextures.ContainsKey(coll)) // if there is already paint on the material, add to that
+                {
+                    Renderer rend = hit.transform.GetComponent<Renderer>();
+                    paintTextures.Add(coll, getWhiteRT());
+                    rend.material.SetTexture("_PaintMap", paintTextures[coll]);
+                }
+                if (stored != hit.lightmapCoord) // stop drawing on the same point
+                {
+                    stored = hit.lightmapCoord;
+                    Vector2 pixelUV = hit.lightmapCoord;
+                    pixelUV.y *= resolution;
+                    pixelUV.x *= resolution;
+                    DrawTexture(paintTextures[coll], pixelUV.x, pixelUV.y);
+                }
+            }
         }
     }
 
-    private IEnumerator DrawShape()
+    void DrawTexture(RenderTexture rt, float posX, float posY)
     {
-        lineRenderer.positionCount = 1;
-        int vertexCount = 1;
-        lineRenderer.SetPosition(0, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        lineRenderer.positionCount++;
 
-        while (isDrawing)
-        {
-            lineRenderer.SetPosition(vertexCount, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            vertexCount++;
-            lineRenderer.positionCount++;
-            yield return new WaitForSeconds(.05f);
-        }
+        RenderTexture.active = rt; // activate rendertexture for drawtexture;
+        GL.PushMatrix();                       // save matrixes
+        GL.LoadPixelMatrix(0, resolution, resolution, 0);      // setup matrix for correct size
+
+        // draw brushtexture
+        Graphics.DrawTexture(new Rect(posX - brushTexture.width / brushSize, (rt.height - posY) - brushTexture.height / brushSize, brushTexture.width / (brushSize * 0.5f), brushTexture.height / (brushSize * 0.5f)), brushTexture);
+        GL.PopMatrix();
+        RenderTexture.active = null;// turn off rendertexture
+
+
+    }
+
+    RenderTexture getWhiteRT()
+    {
+        RenderTexture rt = new RenderTexture(resolution, resolution, 32);
+        Graphics.Blit(whiteMap, rt);
+        return rt;
+    }
+
+    void CreateClearTexture()
+    {
+        whiteMap = new Texture2D(1, 1);
+        whiteMap.SetPixel(0, 0, Color.white);
+        whiteMap.Apply();
     }
 }
