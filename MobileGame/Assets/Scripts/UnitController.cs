23cc5bed832477230cb75bdac31999f3dbf94820
Stijn van der Neut <sne@keygene.com>
2019-11-18 16:52:26 +0100

    Added a dash mechanic that dashes and rotates towards the swipe direction

--- MobileGame/Assets/Scripts/UnitController.cs
+++ MobileGame/Assets/Scripts/UnitController.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class UnitController : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 0.5f;
 
     Vector3 direction;
     Vector3 origin;
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
     GameObject joystickObject;
 
     Animator animator;
+
+    Coroutine dashRoutine;
 
     void Start()
     {
@@ -29,17 +31,19 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (direction.magnitude > 0)
-        {
-            transform.position = transform.position - direction * moveSpeed * Time.fixedDeltaTime;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-direction, Vector3.up), Time.fixedDeltaTime * 10f);
-        }
+        //if (direction.magnitude > 0)
+        //{
+        //    transform.position = transform.position - direction * moveSpeed * Time.fixedDeltaTime;
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-direction, Vector3.up), Time.fixedDeltaTime * 10f);
+        //}
     }
 
     void CheckForMouseInput()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            direction = Vector3.zero;
+            dashRoutine = null;
             origin = Input.mousePosition;
 
             GameObject obj = Instantiate(walkingPrefab, origin, Quaternion.identity, canvas.GetComponent<RectTransform>());
@@ -48,19 +52,46 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("isRunning", true);
         }
 
-        if (Input.GetMouseButton(0))
+        //if (Input.GetMouseButton(0))
+        //{
+        //    direction = origin - Input.mousePosition;
+        //    direction.z = direction.y;
+        //    direction.y = 0f;
+        //    direction.Normalize();
+        //}
+
+        if (Input.GetMouseButtonUp(0))
         {
             direction = origin - Input.mousePosition;
             direction.z = direction.y;
             direction.y = 0f;
             direction.Normalize();
-        }
+            direction *= moveSpeed;
 
-        if (Input.GetMouseButtonUp(0))
-        {
+            Vector3 targetPosition = transform.position - direction;
+
+            if (dashRoutine == null)
+            {
+                dashRoutine = StartCoroutine(DashRoutine(targetPosition, 0.1f));
+            }
+
             animator.SetBool("isRunning", false);
-            direction = Vector3.zero;
             Destroy(joystickObject);
+        }
+    }
+
+    IEnumerator DashRoutine(Vector3 targetPos, float time)
+    {
+        float elapsedTime = 0f;
+        Vector3 startPos = transform.position;
+        Quaternion startRot = transform.rotation;
+
+        while (elapsedTime < time)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, (elapsedTime / time));
+            transform.rotation = Quaternion.Slerp(startRot, Quaternion.LookRotation(-direction, Vector3.up), (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
         }
     }
 }
