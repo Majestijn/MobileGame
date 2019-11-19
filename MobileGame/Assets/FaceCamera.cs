7b89aa821e175f83dc59a8da1a9eee121c6a445b
Stijn van der Neut <sne@keygene.com>
2019-11-19 16:05:32 +0100

    Added a healthbar to the player that follows it and is always rotated towards the camera

---
+++ MobileGame/Assets/FaceCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position, Vector3.up);
+        lookRotation.y = 0f;
+        lookRotation.z = 0f;
+        lookRotation.w = 0f;
+
+        transform.rotation = lookRotation;
+    }
+}
---
+++ MobileGame/Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    
+    [SerializeField] private Transform playerToFollow;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
